6d8b3a3c275da24387474569e5a9d6879d4c6562
Linh Phan Duy Tung <phanduytunglinh@users.noreply.github.com>
2016-08-10 16:45:19 +0700

    Add files via upload

---
+++ Android setting screen.mm
@@ -0,0 +1,74 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1470707821684" ID="ID_529662117" MODIFIED="1470822265871" STYLE="fork" TEXT="Android setting screen">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1470708126582" HGAP="35" ID="ID_1295466154" MODIFIED="1470822265847" POSITION="right" STYLE="fork" TEXT="Preference types" VSHIFT="-101">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1470708144536" ID="ID_1368591927" MODIFIED="1470822265851" STYLE="fork" TEXT="CheckBoxPreference">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470708155103" ID="ID_1989654589" MODIFIED="1470822265853" STYLE="fork" TEXT="EditTextPreference">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470708162424" ID="ID_1063438583" MODIFIED="1470822265854" STYLE="fork" TEXT=" ListPreference">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470708166592" ID="ID_1940714454" MODIFIED="1470822265856" STYLE="fork" TEXT="MultiSelectListPreference">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470708188216" ID="ID_1511055765" MODIFIED="1470822265857" STYLE="fork" TEXT="SwitchPreference">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470708204312" ID="ID_283879055" MODIFIED="1470822265859" STYLE="fork" TEXT="active an Intent">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1470713069563" HGAP="29" ID="ID_1771131437" MODIFIED="1470822265861" POSITION="right" STYLE="fork" TEXT="Setting group approach" VSHIFT="-144">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1470713082320" ID="ID_593099558" MODIFIED="1470822265863" STYLE="fork" TEXT="By title - PreferenceCategory ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470713118767" ID="ID_1427169053" MODIFIED="1470822265863" STYLE="fork" TEXT="By screen - PreferenceScreen  ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1470713456528" HGAP="55" ID="ID_443163053" MODIFIED="1470822265864" POSITION="right" STYLE="fork" TEXT="Screen design approach" VSHIFT="3">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1470714378832" ID="ID_524303975" MODIFIED="1470822265865" STYLE="fork" TEXT="Preference Activity">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470714381097" ID="ID_215850119" MODIFIED="1470822265867" STYLE="fork" TEXT="Preference Fragments">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1470714997538" ID="ID_1535019044" MODIFIED="1470822265868" STYLE="fork" TEXT="Using preference headers">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1470715563313" ID="ID_612645573" MODIFIED="1470822265868" STYLE="fork" TEXT=" use tag &lt;extra&gt; to pass data to fragment">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1470794186924" ID="ID_1844180741" MODIFIED="1470822265870" POSITION="right" STYLE="fork" TEXT="support &lt; Android 3 ?">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1470822025717" ID="ID_442850853" MODIFIED="1470822265871" STYLE="fork" TEXT="use Preference to active an Intent that will open new fragment or activity">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
